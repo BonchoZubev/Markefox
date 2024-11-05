d5895ad7f64b7ee6375fc840cd604def314199f9
c0mrade <160029204+BonchoZubev@users.noreply.github.com>
2024-11-05 18:19:14 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,6 @@
+var Typed = new Typed(".auto-type", {
+    strings: ["HEY, WE'RE MARKEFOX", "", "HEY, WE'RE MARKEFOX", "", "HEY, WE'RE MARKEFOX"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
